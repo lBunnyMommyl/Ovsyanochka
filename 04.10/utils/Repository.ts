fe82729fe5a6c2792ad3a155d2442808ed05ec20
lBunnyMommyl <super.pan-10@ya.ru>
2025-10-13 19:21:16 +0000

    Задание 04.10 часть 2

---
+++ 04.10/utils/Repository.ts
@@ -0,0 +1,19 @@
+export class Repository<T> {
+  private items: T[];
+
+  constructor(initialItems: T[] = []) {
+    this.items = [...initialItems];
+  }
+
+  add(item: T): void {
+    this.items.push(item);
+  }
+
+  getAll(): T[] {
+    return [...this.items];
+  }
+}
+
+export function getReadonlyBooks<T>(repo: Repository<T>): Readonly<T[]> {
+  return repo.getAll() as Readonly<T[]>;
+}
